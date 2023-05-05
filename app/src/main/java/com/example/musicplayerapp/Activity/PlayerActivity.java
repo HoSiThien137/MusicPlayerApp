db1c927b56f21e2d8f4a6861b22c16d0ff361d86
HoSiThien137 <118470903+HoSiThien137@users.noreply.github.com>
2023-05-06 02:06:14 +0700

    layout

---
+++ app/src/main/java/com/example/musicplayerapp/Activity/PlayerActivity.java
@@ -0,0 +1,15 @@
+package com.example.musicplayerapp.Activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.musicplayerapp.R;
+
+public class PlayerActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_player);
+    }
+}
---
+++ app/src/main/java/com/example/musicplayerapp/Activity/SignUpActivity.java
@@ -0,0 +1,17 @@
+package com.example.musicplayerapp.Activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.musicplayerapp.R;
+
+public class SignUpActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle saveInstanceState) {
+        super.onCreate(saveInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
